7eba911184f2a7db9479b7473f4e07e3693afa20
christian_burger <christian_burger@yahoo.com>
2024-03-15 20:36:56 +0100

    initial

---
+++ src/main/java/com/runtracer/runtracerbackend/RuntracerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.runtracer.runtracerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntracerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntracerBackendApplication.class, args);
+	}
+
+}
