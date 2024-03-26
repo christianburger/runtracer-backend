796115d50739266ae0783ad3ee1b2b3544be8769
christian_burger <christian_burger@yahoo.com>
2024-03-26 19:28:09 +0100

    Small fix on main test.

--- src/test/java/com/runtracer/runtracerbackend/RuntracerBackendApplicationTests.java
+++ src/test/java/com/runtracer/runtracerbackend/RuntracerBackendApplicationTests.java
@@ -1,15 +1,10 @@
 package com.runtracer.runtracerbackend;
 
-import com.runtracer.runtracerbackend.repository.ReactiveOAuth2ClientRegistrationRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RuntracerBackendApplicationTests {
-
-    @Autowired
-    private ReactiveOAuth2ClientRegistrationRepository reactiveOAuth2ClientRegistrationRepository;
 
     @Test
     void contextLoads() {
